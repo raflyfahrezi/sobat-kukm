02a46383b1b1a2cdf614e46af11b504794b778dd
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-05-05 17:41:52 +0700

    fix login script

--- js/pages/login.js
+++ js/pages/login.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+const setPasswordListener = () => {
     const passwordToggle = document.getElementById('passwordToggle')
     const passwordElement = document.getElementsByName('password')[0]
 
@@ -17,4 +17,30 @@ document.addEventListener('DOMContentLoaded', () => {
             passwordElement.setAttribute('type', 'password')
         }
     })
+}
+
+const setConfirmPasswordListener = () => {
+    const passwordToggle = document.getElementById('confirmPasswordToggle')
+    const passwordElement = document.getElementsByName('confirmPassword')[0]
+
+    if (!passwordToggle) {
+        return
+    }
+
+    passwordToggle.addEventListener('click', () => {
+        const type = passwordElement.getAttribute('type')
+
+        console.log(type)
+
+        if (type === 'password') {
+            passwordElement.setAttribute('type', 'text')
+        } else {
+            passwordElement.setAttribute('type', 'password')
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setPasswordListener()
+    setConfirmPasswordListener()
 })
