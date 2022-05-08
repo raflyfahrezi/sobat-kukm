9be7dc23c7d91741ccb6a2bbdb04af6aeb4178f8
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-05-08 12:28:45 +0700

    fix nav script

--- js/components/navigation.js
+++ js/components/navigation.js
@@ -10,7 +10,66 @@ const setNavigationToScrolled = () => {
     navigationClass.remove('nav_scrolled')
 }
 
+const NavMenuButtonExitListener = () => {
+    const navMenuExitButton = document.getElementById('nav_menu_button_exit')
+
+    if (!navMenuExitButton) {
+        return
+    }
+
+    navMenuExitButton.addEventListener('click', () => {
+        closeNavigation()
+    })
+}
+
+const NavLinkItemListener = () => {
+    const navLinkItem = document.getElementsByClassName(
+        'nav_link_item_resposive'
+    )
+
+    for (let index = 0; index < navLinkItem.length; index++) {
+        navLinkItem[index].addEventListener('click', () => {
+            closeNavigation()
+        })
+    }
+}
+
+const NavMenuButtonListener = () => {
+    const navMenuButton = document.getElementById('nav_menu_button')
+
+    if (!navMenuButton) {
+        return
+    }
+
+    navMenuButton.addEventListener('click', () => {
+        openNavigation()
+    })
+}
+
+const openNavigation = () => {
+    const responsiveNavigation = document.getElementById('navigationResponsive')
+
+    responsiveNavigation.style.display = 'block'
+
+    setTimeout(() => {
+        responsiveNavigation.style.opacity = 1
+        NavMenuButtonExitListener()
+        NavLinkItemListener()
+    }, 100)
+}
+
+const closeNavigation = () => {
+    const responsiveNavigation = document.getElementById('navigationResponsive')
+
+    responsiveNavigation.style.opacity = 0
+
+    setTimeout(() => {
+        responsiveNavigation.style.display = 'none'
+    }, 100)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    NavMenuButtonListener()
     setNavigationToScrolled()
 
     window.addEventListener('scroll', setNavigationToScrolled)
